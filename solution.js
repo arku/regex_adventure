fac2798395c5a89912c1a6c18d663963e6999f82
Arun <arunkumarm1995@gmail.com>
2016-06-12 01:41:18 +0530

    Initial commit

---
+++ solution.js
@@ -0,0 +1,22 @@
+/*module.exports = function(str){
+    //Returns whether the string contains LITERALLY
+    return /LITERALLY/.test(str)
+}*/
+
+/*module.exports = function(str){
+    //Returns whether the string starts with LITERALLY
+    return /^LITERALLY/.test(str)
+}*/
+
+/*module.exports = function (str) {
+    // return whether str ends with "BANANAS"
+    return /BANANAS$/.test(str);
+}*/
+
+/*module.exports = function (str) {
+    // return whether str starts with a vowel or a digit 
+    return /^[aeiou0-9]/.test(str);
+}*/
+module.exports = function(str) {
+    //return true when first character is not a vowel
+}
